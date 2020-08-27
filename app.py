54d446b03a1cb1032fd2d4674689d4c39ca6c68d
dobothor <70290147+dobothor@users.noreply.github.com>
2020-08-27 12:04:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ import pandas
 import dash_table
 
 global G
-G = pandas.read_csv(r"C:\Users\Thor\Dropbox\dashboard\web-app\G_new.csv")
-#url = "https://raw.githubusercontent.com/dobothor/spatial-gini/master/G_new.csv"
-#G = pandas.read_csv(url)
+#G = pandas.read_csv(r"C:\Users\Thor\Dropbox\dashboard\web-app\G_new.csv")
+url = "https://raw.githubusercontent.com/dobothor/spatial-gini/master/G_new.csv"
+G = pandas.read_csv(url)
 G.iloc[0:111,1:33]=G.iloc[0:111,1:33].applymap(lambda x: round(x,3))
 #could use better way to round numbers...
 print(G.head())
